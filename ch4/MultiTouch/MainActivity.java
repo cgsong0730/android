3afc46aafbecb2a2c6af3bf315f1f1ff294dfd23
cgsong0730 <48252454+cgsong0730@users.noreply.github.com>
2021-09-30 03:28:02 +0900

    Create MainActivity.java

---
+++ ch4/MultiTouch/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.eventproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(new MultiTouchView(this, null));
+    }
+}
