1558749b7b12f8c519ca4fa24c0947cf08d63407
cgsong0730 <48252454+cgsong0730@users.noreply.github.com>
2021-10-28 03:00:01 +0900

    Create Fragment2.java

---
+++ ch7/Fragment/Fragment2.java
@@ -0,0 +1,20 @@
+package com.example.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class Fragment2 extends Fragment {
+     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment2, container, false);
+    }
+}
