0bec8cf5ec5369bb1b140276af86c225b91a5a30
cgsong0730 <48252454+cgsong0730@users.noreply.github.com>
2021-10-27 21:18:39 +0900

    Create MainActivity.java

---
+++ ch7/GridView/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.gridview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        GridView gridView = (GridView) findViewById(R.id.gridView1);
+        gridView.setAdapter(new ImageAdapter(this));
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getBaseContext(), ""+position, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
