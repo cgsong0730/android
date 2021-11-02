b778e9de51bb1c0e0e336899bf9225c04172136b
cgsong0730 <48252454+cgsong0730@users.noreply.github.com>
2021-11-02 23:30:59 +0900

    Update MainActivity.java

--- ch6/Paint/MainActivity.java
+++ ch6/Paint/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,12 +16,17 @@ public class MainActivity extends AppCompatActivity {
     ImageButton redButton, greenButton, blueButton;
 
     DrawingPaper drawingPaper;
+    int currentTool = 1; // 1 color, 2 brush
+    LinearLayout colorLayout, brushLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         drawingPaper = new DrawingPaper(this);
+
+        colorLayout = findViewById(R.id.color_list);
+        brushLayout = findViewById(R.id.brush_list);
 
         redButton = findViewById(R.id.red_button);
         greenButton = findViewById(R.id.green_button);
@@ -30,6 +36,22 @@ public class MainActivity extends AppCompatActivity {
         eraserButton = findViewById(R.id.eraser_button);
         frame = findViewById(R.id.frame_container);
         frame.addView(drawingPaper);
+        setBottomList(currentTool);
+    }
+
+    public void setBottomList (int toolNumber) {
+        currentTool = toolNumber;
+
+        switch (currentTool) {
+            case 1:
+                colorLayout.setVisibility(View.VISIBLE);
+                brushLayout.setVisibility(View.GONE);
+                break;
+            case 2:
+                colorLayout.setVisibility(View.GONE);
+                brushLayout.setVisibility(View.VISIBLE);
+                break;
+        }
     }
 
     public void onPoubelleButton (View view) {
@@ -38,6 +60,28 @@ public class MainActivity extends AppCompatActivity {
 
     public void onEraserButton (View view) {
         drawingPaper.setEraser();
+    }
+
+    public void onColorToolButton(View view) {
+        setBottomList(1);
+    }
+
+    public void onBrushButton(View view) {
+        setBottomList(2);
+    }
+
+    public void onBrushSizeButton(View view) {
+        switch (view.getId()) {
+            case R.id.small_button:
+                drawingPaper.setPaintStrokeWidth(10f);
+                break;
+            case R.id.middle_button:
+                drawingPaper.setPaintStrokeWidth(20f);
+                break;
+            case R.id.big_button:
+                drawingPaper.setPaintStrokeWidth(30f);
+                break;
+        }
     }
 
     public void onColorButton (View view) {
