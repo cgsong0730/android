cccb273876a74fc4a9ff2ccbb4d7541e26ca755c
cgsong0730 <48252454+cgsong0730@users.noreply.github.com>
2021-11-16 04:46:30 +0900

    Create MainActivity.java

---
+++ ch11/DatabaseAdapter/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.databaseadapter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+
+public class MainActivity extends AppCompatActivity {
+
+    DatabaseHelper helper;
+    SQLiteDatabase db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        helper = new DatabaseHelper(this);
+        try {
+            db = helper.getWritableDatabase();
+        } catch (SQLiteException ex) {
+            db = helper.getReadableDatabase();
+        }
+        Cursor cursor = db.rawQuery("SELECT * FROM contacts", null);
+
+        startManagingCursor(cursor);
+
+        String[] from = {"name", "tel"};
+        int[] to = {android.R.id.text1, android.R.id.text2};
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(this,
+                android.R.layout.simple_list_item_2, cursor, from, to);
+        ListView list = (ListView) findViewById(R.id.list);
+        list.setAdapter(adapter);
+    }
+}
