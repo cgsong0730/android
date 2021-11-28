4eaf631ca1322863ea563c3308b23a1ab9458844
cgsong0730 <48252454+cgsong0730@users.noreply.github.com>
2021-11-29 07:58:35 +0900

    Update MainActivity.java

--- ch11/MovieDatabase/MainActivity.java
+++ ch11/MovieDatabase/MainActivity.java
@@ -29,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, SubActivity.class);
+                intent.putExtra("ACTION_REQUEST", "CREATE");
                 startActivityForResult(intent, GET_STRING);
             }
         });
@@ -47,18 +48,37 @@ public class MainActivity extends AppCompatActivity {
         startManagingCursor(cursor);
 
         String[] from = {"name", "year", "pd", "score", "nation"};
-
         int[] to = {R.id.name, R.id.year, R.id.pd, R.id.score, R.id.nation};
+
         SimpleCursorAdapter adapter = new SimpleCursorAdapter(this,
                 R.layout.listitem, cursor, from, to);
-        ListView list = (ListView) findViewById(R.id.list);
 
+        ListView list = (ListView) findViewById(R.id.list);
         list.setAdapter(adapter);
         list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Intent intent = new Intent(MainActivity.this, SubActivity.class);
-                startActivityForResult(intent, GET_STRING);
+                intent.putExtra("ACTION_REQUEST", "UPDATE");
+                Cursor cursor = db.rawQuery("SELECT * FROM contacts WHERE _id = "
+                                + id + ";"
+                        , null);
+
+                if (cursor.moveToFirst()) {
+                    String rowId = cursor.getString(cursor.getColumnIndex("_id"));
+                    String name = cursor.getString(cursor.getColumnIndex("name"));
+                    String year = cursor.getString(cursor.getColumnIndex("year"));
+                    String pd = cursor.getString(cursor.getColumnIndex("pd"));
+                    String score = cursor.getString(cursor.getColumnIndex("score"));
+                    String nation = cursor.getString(cursor.getColumnIndex("nation"));
+                    intent.putExtra("ID", rowId);
+                    intent.putExtra("NAME", name);
+                    intent.putExtra("YEAR", year);
+                    intent.putExtra("PD", pd);
+                    intent.putExtra("SCORE", score);
+                    intent.putExtra("NATION", nation);
+                    startActivityForResult(intent, GET_STRING);
+                }
             }
         });
     }
@@ -66,15 +86,37 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == GET_STRING && resultCode == RESULT_OK) {
-            db.execSQL("INSERT INTO contacts VALUES (null, " +
-                    "'" +data.getStringExtra("INPUT_NAME")+ "', "+
-                    "'" +data.getStringExtra("INPUT_YEAR")+ "', "+
-                    "'" +data.getStringExtra("INPUT_PD")+ "', "+
-                    "'" +data.getStringExtra("INPUT_SCORE")+ "', "+
-                    "'" +data.getStringExtra("INPUT_NATION")+ "');");
-            printDb();
-        }
-        else {
+
+            switch (data.getStringExtra("ACTION_RESULT")) {
+                case "CREATE":
+                    db.execSQL("INSERT INTO contacts VALUES (null, " +
+                            "'" + data.getStringExtra("INPUT_NAME") + "', " +
+                            "'" + data.getStringExtra("INPUT_YEAR") + "', " +
+                            "'" + data.getStringExtra("INPUT_PD") + "', " +
+                            "'" + data.getStringExtra("INPUT_SCORE") + "', " +
+                            "'" + data.getStringExtra("INPUT_NATION") + "');"
+                    );
+                    printDb();
+                    break;
+                case "UPDATE":
+                    db.execSQL("UPDATE contacts SET " +
+                            "name = '" + data.getStringExtra("INPUT_NAME") + "', " +
+                            "year = '" + data.getStringExtra("INPUT_YEAR") + "', " +
+                            "pd = '" + data.getStringExtra("INPUT_PD") + "', " +
+                            "score = '" + data.getStringExtra("INPUT_SCORE") + "', " +
+                            "nation = '" + data.getStringExtra("INPUT_NATION") + "' " +
+                            "WHERE _id = '" + data.getStringExtra("ID") + "';"
+                    );
+                    printDb();
+                    break;
+                case "DELETE":
+                    db.execSQL("DELETE FROM contacts WHERE _id = " +
+                            "'" + data.getStringExtra("ID") + "';"
+                    );
+                    printDb();
+                    break;
+            }
+        } else {
             super.onActivityResult(requestCode, resultCode, data);
         }
     }
