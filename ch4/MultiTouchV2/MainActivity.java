636e77e1448771da8822c43be331d2c7af0d834d
cgs <48252454+cgsong0730@users.noreply.github.com>
2024-10-10 14:59:51 +0900

    Create MainActivity.java

---
+++ ch4/MultiTouchV2/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.multitouchv2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(new MultiTouchView(this, null));
+    }
+
+}
