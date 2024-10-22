70c1ff75a78c7c286f6448fda0729e85cb05e023
cgs <48252454+cgsong0730@users.noreply.github.com>
2024-10-23 02:40:15 +0900

    Update MainActivity.java

--- ch4/SwitchTemperatureV2/MainActivity.java
+++ ch4/SwitchTemperatureV2/MainActivity.java
@@ -3,9 +3,7 @@ package com.example.pro2;
 
 import android.os.Bundle;
 
-
 import androidx.appcompat.app.AppCompatActivity;
-
 
 import android.view.View;
 import android.widget.EditText;
@@ -13,39 +11,31 @@ import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
-
 public class MainActivity extends AppCompatActivity {
-
 
    RadioButton radio1;
    RadioButton radio2;
    TextView unit;
    EditText input1;
 
-
    @Override
    protected void onCreate(Bundle savedInstanceState) {
        super.onCreate(savedInstanceState);
        setContentView(R.layout.activity_main);
-
 
        radio1 = findViewById(R.id.radio1);
        radio2 = findViewById(R.id.radio2);
        unit = findViewById(R.id.unit);
        input1 = findViewById(R.id.input1);
 
-
        radio1.setChecked(true);
    }
 
-
    public void onButtonClick(View view) {
-
 
        try {
            Float temp1 = Float.parseFloat(input1.getText().toString());
            Float temp2 = 0.0f; // /33.8 or *33.8
-
 
            if (radio1.isChecked()) { // 섭씨
                temp2 = temp1 / 33.8f;
@@ -63,7 +53,6 @@ public class MainActivity extends AppCompatActivity {
            Toast.makeText(getApplicationContext(), "숫자를 입력해주세요.", Toast.LENGTH_SHORT).show();
        }
    }
-
 
    public void onButtonReset(View view) {
        radio1.setChecked(true);
