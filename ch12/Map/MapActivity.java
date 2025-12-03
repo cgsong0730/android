ecb320024094bd3d2765d5296721ac2524691738
cgs <security0730@korea.ac.kr>
2025-12-03 23:59:26 +0900

    Implement MapActivity with Google Maps integration

---
+++ ch12/Map/MapActivity.java
@@ -0,0 +1,66 @@
+package com.example.pro2;
+
+import androidx.fragment.app.FragmentActivity;
+
+import android.Manifest;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.MapsInitializer.Renderer;
+import com.google.android.gms.maps.OnMapsSdkInitializedCallback;
+
+public class MapActivity extends FragmentActivity implements OnMapReadyCallback, OnMapsSdkInitializedCallback {
+
+    private GoogleMap mMap;
+
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1000;
+    private static final String[] REQUIRED_PERMISSIONS = new String[] {
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        MapsInitializer.initialize(getApplicationContext(), Renderer.LATEST, this);
+        setContentView(R.layout.activity_map);
+
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(this);
+        } else {
+            Log.d("MapsDemo", "Can not find Map Fragment.");
+        }
+    }
+
+    @Override
+    public void onMapsSdkInitialized(MapsInitializer.Renderer renderer) {
+        switch (renderer) {
+            case LATEST:
+                Log.d("MapsDemo", "The latest version of the renderer is used.");
+                break;
+            case LEGACY:
+                Log.d("MapsDemo", "The legacy version of the renderer is used.");
+                break;
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        LatLng seoul = new LatLng(37.5665, 126.9780);
+        mMap.addMarker(new MarkerOptions().position(seoul).title("서울"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(seoul, 12f));
+
+    }
+}
